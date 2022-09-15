49e0c31c91e0ae2090312693ffddcb40a893ad2e
Meghan Cowan <meghancowan@microsoft.com>
2022-09-15 13:27:23 -0700

    hierarchical amd allreduce

--- examples/scclang/allreduce_mi200_hierarchical.py
+++ examples/scclang/allreduce_mi200_hierarchical.py
@@ -19,50 +19,62 @@ rings = [
     [0,1,9,8,12,13,15,14,10,11,3,2,6,7,5,4],
     [0,8,9,13,12,14,15,11,10,2,3,7,6,4,5,1],
     [0,1,5,4,6,7,3,2,10,11,15,14,12,13,9,8],
+    [0,8,9,13,12,14,15,11,10,2,3,7,6,4,5,1],
+    [0,1,5,4,6,7,3,2,10,11,15,14,12,13,9,8],
+    [0,4,5,6,7,3,2,9,8,12,13,14,15,11,10,1],
+    [0,1,10,11,15,14,13,12,8,9,2,3,7,6,5,4],
 ]
 
 # r = ring index
 # g = gpu index
-def rank(r, n, g):
+def ring2rank(r, n, g):
     return rings[r][g] + n * num_local_gpus
 
+def rank(n, g):
+    return n * num_local_gpus + g
         
 def allreduce_ring(num_nodes, instances, protocol, schedule):
     size = num_nodes * num_local_gpus
-    chunks_per_node = len(rings)
-    num_chunks = chunks_per_node * num_nodes
+    num_rings = len(rings)
+    num_chunks = num_rings* num_local_gpus
     topology = fully_connected(size)
     collective = AllReduce(size, num_chunks, True)
-    offset=1
 
     with SCCLProgram("allreduce_ring_mi200", topology, collective, instances,
-        protocol=protocol, threadblock_policy=ThreadblockPolicy.auto, interleaved_replication=False, instr_fusion=True):
+        protocol=protocol, threadblock_policy=ThreadblockPolicy.manual, interleaved_replication=False, instr_fusion=True):
         # Intra-node reduce scatter
+        rank2chunk = defaultdict(list)
         for n in range(num_nodes):
-            for index in range(num_chunks):        
-                x = index % chunks_per_node
-                c = chunk(rank(x, n, (x+offset)%num_local_gpus), Buffer.input, index)
-                for step in range(1, num_local_gpus):
-                    c = chunk(rank(x, n, (x+offset+step)%num_local_gpus), Buffer.input, index).reduce(c)
+            for g in range(num_local_gpus):
+                for r in range(num_rings):
+                    index = rings[r].index(g)
+                    offset = r * num_local_gpus + g
+                    c = chunk(ring2rank(r, n, index), Buffer.input, offset)
+                    # ch = r+(offset%channels)*12
+                    for step in range(1, num_local_gpus):
+                        c = chunk(ring2rank(r, n, (index+step)%num_local_gpus), Buffer.input, offset).reduce(c, sendtb=r, recvtb=r, ch=r)
+                    if n == 0:
+                        rank2chunk[c.rank].append(c.index)
 
         # Inter-node allreduce (reduce scatter + allgather)
-        for index in range(num_chunks):
-            g = index % chunks_per_node
-            n = index // chunks_per_node
-            c = chunk(rank(g, n, g), Buffer.input, index)
-            for step in range(1, num_nodes):
-                c = chunk(rank(g, (n+step)%num_nodes, g), Buffer.input, index).reduce(c)
-            for step in range(0, num_nodes-1):
-                c = c.copy(rank(g, (n+step)%num_nodes, g), Buffer.input, index) 
+        for g in range(num_local_gpus):
+            for i, index in enumerate(rank2chunk[g]):
+                n = i % num_nodes
+                c = chunk(rank(n, g), Buffer.input, index)
+                for step in range(1, num_nodes):
+                    c = chunk(rank((n+step)%num_nodes, g), Buffer.input, c.index).reduce(c, sendtb=12+n, recvtb=12+n, ch=n)
+                for step in range(0, num_nodes-1):
+                    c = c.copy(rank((n+step)%num_nodes, g), Buffer.input, c.index, sendtb=12+n, recvtb=12+n, ch=n)        
 
         # Intra-node allgather
         for n in range(num_nodes):
-            for index in range(num_chunks):        
-                g = index % chunks_per_node
-                c = chunk(rank(g, n, g), Buffer.input, index)
-                for step in range(1, num_local_gpus):
-                    c = c.copy(rank(g, n, (g+step)%num_local_gpus), Buffer.input, index)
-
+            for g in range(num_local_gpus):
+                for r in range(num_rings):
+                    index = (rings[r].index(g) - 1) % num_local_gpus
+                    offset = r * num_local_gpus + g
+                    c = chunk(ring2rank(r, n, index), Buffer.input, offset)
+                    for step in range(1, num_local_gpus):
+                        c = c.copy(ring2rank(r, n, (index+step)%num_local_gpus), Buffer.input, offset, sendtb=12+num_nodes+r, recvtb=12+num_nodes+r, ch=r+12)          
         XML()
         Check()
 
@@ -75,4 +87,5 @@ parser.add_argument('--schedule', type=str, default='auto', choices=['auto', 'ma
 args = parser.parse_args()
 
 assert args.num_nodes > 1, "Multi-node allreduce. num_nodes > 1"
+assert 12 % args.num_nodes == 0, "Algorithm requires the number of nodes to divide 12"
 allreduce_ring(args.num_nodes, args.instances, args.protocol, args.schedule)
